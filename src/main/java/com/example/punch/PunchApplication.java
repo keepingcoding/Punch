6bd2e0b250bd62490f77f98b159f49c5df8948f6
zzs <46862869+keepingcoding@users.noreply.github.com>
2019-10-24 16:24:18 +0800

    initial commit

---
+++ src/main/java/com/example/punch/PunchApplication.java
@@ -0,0 +1,23 @@
+package com.example.punch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * @author zzs
+ * @date 2019/10/24 16:00
+ */
+@SpringBootApplication
+@PropertySource(value = {"classpath:config.properties"}, ignoreResourceNotFound = true)
+public class PunchApplication {
+
+    public static void main(String[] args) {
+        //开启log日志彩色显示
+        System.setProperty("log4j.skipJansi", "false");
+
+        SpringApplication.run(PunchApplication.class, args);
+    }
+
+}
+
